b865a01bd68f0db614ed2a80646871200c1ae1b4
NatsuCamellia <natsucamellia@gmail.com>
2025-07-21 17:02:18 +0800

    Implement q_insert_head to insert a new element
    Allocate a new element for the given string and insert it to the queue.
    Return false if head or s is NULL or allocation failed.
    
    Change-Id: I947529f068a94e3adb0cb6261c05d257d88e4daf

--- queue.c
+++ queue.c
@@ -21,6 +21,21 @@ void q_free(struct list_head *head) {}
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+
+    element_t *e = malloc(sizeof(element_t));
+    if (!e)
+        return false;
+
+    e->value = strdup(s);
+    if (!e->value) {
+        free(e);
+        e = NULL;
+        return false;
+    }
+
+    list_add(&e->list, head);
     return true;
 }
 
